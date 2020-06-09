fd38d13feb6fed25647a23b267327679ff93ad3a
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-06-09 21:45:17 +0530

    Add files via upload

---
+++ tic-tac-toe/tic-tac-toe.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+export class Square extends Component {
+  render() {
+    return <button className="square">{this.props.value}</button>;
+  }
+}
+
+export class Board extends Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+
+  render() {
+    const status = "Player: X";
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export class Game extends Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+      </div>
+    );
+  }
+}
