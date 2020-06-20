50f08e2ca4b1dd411c163ab1ebb7c89ebf901c0b
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-06-21 01:29:47 +0530

    Create table.jsx

---
+++ table.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Columns from './columns';
+
+function Table(){
+    return(
+        <table>
+            <tbody>
+                <tr>
+                    <Columns/>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default Table;
