175fd0fc21984e9a7cf6ee5dc4217dc1dd470a79
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-06-28 02:29:10 +0530

    Add files via upload

---
+++ Hooks/HookCounter2.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState } from "react";
+
+const HookCounterTwo = () => {
+  const initialCount = 0;
+  const [count, setCount] = useState(initialCount);
+
+  const incrementFive = () => {
+    for (let i = 0; i < 5; i++) {
+      setCount((prevCount) => prevCount + 1);
+    }
+  };
+  return (
+    <div>
+      Count:{count}
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+        Decrement
+      </button>
+      <button onClick={incrementFive}>incrementFive</button>
+    </div>
+  );
+};
+
+export default HookCounterTwo;
