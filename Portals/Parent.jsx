4500d6f09071ff01749f290cd0e8feb7fc09e4d7
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-08-03 11:28:40 +0530

    Update Parent.jsx

--- Portals/Parent.jsx
+++ Portals/Parent.jsx
@@ -1,28 +1,53 @@
 import React, { useState, Component } from "react";
 import Modal from "./Modal";
+import "./portal.css";
 
-const Parent = () => {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("");
-  return (
-    <>
-      <div onClick={() => setCount(count + 1)}>No.of Clicks:{count}</div>
-      <p>
-        Name:
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <br />
-        <strong>{name}</strong>
-      </p>
-      <Modal>
-        <Child />
-      </Modal>
-    </>
-  );
-};
+class Parent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { clicks: 0 };
+  }
+
+  handleClick = () => {
+    this.setState({
+      clicks: this.state.clicks + 1,
+    });
+  };
+
+  render() {
+    return (
+      <div onClick={this.handleClick}>
+        <p>Number of clicks: {this.state.clicks}</p>
+        <Modal>
+          <Child />
+        </Modal>
+      </div>
+    );
+  }
+}
+
+// const Parent = () => {
+//   const [count, setCount] = useState(0);
+//   const [name, setName] = useState("");
+//   return (
+//     <>
+//       <div onClick={() => setCount(count + 1)}>No.of Clicks:{count}</div>
+//       <p>
+//         Name:
+//         <input
+//           type="text"
+//           value={name}
+//           onChange={(e) => setName(e.target.value)}
+//         />
+//         <br />
+//         <strong>{name}</strong>
+//       </p>
+//       <Modal>
+//         <Child />
+//       </Modal>
+//     </>
+//   );
+// };
 
 function Child() {
   return (
